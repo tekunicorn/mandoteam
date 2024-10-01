a2afd0c7c1061e87f964c3b3814964911513a8dc
DOĞAN KASAPOĞLU <84666363+tekunicorn@users.noreply.github.com>
2024-10-01 21:49:40 +0300

    Update kizgin_ic.js

---
+++ kizgin_ic.js
@@ -0,0 +1,162 @@
+   var quotes = new Array();
+quotes[0] = "<p id='baslik' title='Windows kısayolı 1'>Bir sekmeyi yanlışlıkla mı kapattınız?</p><br><p id='aciklama'>En son kapatılan sekmeyi yeniden açmak ve yaptığınız şeye geri dönmek için kısayol tuşları.<br>" +
+    "<strong>Ctrl + Shift + T" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_ctrl'>Ctrl</span>" +
+    "<span class='highlight left_shift'>Shift</span>" +
+    "<span class='highlight t'>T</span>" +
+    "</span></strong></p>";
+quotes[1] = "<p id='baslik' title='Windows kısayolı 2'>Pencere yakalama ve çoklu monitör kontrolü</p> <br> <p id='aciklama'><strong id='kyb'>Windows Tuşu + Ok" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight left_ok'>←</span>" +
+    "<span class='highlight right_ok'>→</span>" +
+    "</span>" +
+    "</strong> Tuşlarına basmak, bir pencerenin her iki monitörün her iki tarafına da hızlı bir şekilde oturmasına neden olur. Alternatif olarak,<br /><strong id='kyb' title='Shift + Windows Tuşu + Ok'>Shift + Windows Tuşu + Ok"+
+    "<span class='tooltip'>" +
+	"<span class='highlight left_win'>Win</span>"+
+"<span class='highlight down_ok'>↓</span>"+
+"<span class='highlight up_ok'>↑</span>"+
+"<span class='highlight left_ok'>←</span>"+
+"<span class='highlight left_shift'>Shift</span>"+
+"<span class='highlight left_win'>Win</span>"+
+"<span class='highlight right_ok'>→</span>"+
+"</strong> tuşlarına basmak, pencerenin diğer monitöre geçmesine neden olur. <strong id='kyb' title='Windows + P'>Windows + P"+
+    "<span class='tooltip'>" +
+"<span class='highlight left_win'>Win</span>"+
+"<span class='highlight p'>P</span></strong> tuşlarına basarken, hızlı bir şekilde ikinci bir ekran veya projektör kurmanıza olanak tanır.</p>";
+
+quotes[2] = "<p id='baslik' title='Windows kısayolı 3'>Yanlışlıkla sildiğiniz dosya yazı vs..</p> <br><p id='aciklama'>Bir dosyayı yanlışlıkla siler veya taşırsanız, onu olduğu yere geri getirmek için <strong id='kyb'>Ctrl + Z" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_ctrl'>Ctrl</span>" +
+    "<span class='highlight z'>Z</span>" +
+    "</span>" +
+    "</strong> tuşlarına basabilirsiniz <strong id='kyb'>Ctrl + Y" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_ctrl'>Ctrl</span>" +
+    "<span class='highlight y'>Y</span>" +
+    "</span>" +
+    "</strong>, geri aldığınızı geri gönderir.</p>";
+
+quotes[3] = "<p id='baslik' title='Windows kısayolı 4'>YouTube klavye kısayolları</p> <br><p id='aciklama'>Bir YouTube videosunu duraklatmak için boşluk çubuğunu kullanmanın etkili olduğunu düşünüyorsanız (odak başka bir yerde olduğunda ve çalışmıyorsa), bunun yerine kullanmayı deneyin <strong id='kyb'>K" +
+    "<span class='tooltip'>" +
+    "<span class='highlight k'>K</span>" +
+    "</span>" +
+    "</strong> duraklatmak için, <strong id='kyb'>J" +
+    "<span class='tooltip'>" +
+    "<span class='highlight j'>J</span>" +
+    "</span>" +
+    "</strong> ve <strong id='kyb'>L" +
+    "<span class='tooltip'>" +
+    "<span class='highlight l'>L</span>" +
+    "</span>" +
+    "</strong> 10 saniye geri/ileri adım atacaktır. <strong id='kyb'>M" +
+    "<span class='tooltip'>" +
+    "<span class='highlight m'>M</span>" +
+    "</span>" +
+    "</strong> susturmak için çalışır. Süper kullanışlı.</p>";
+
+quotes[4] = "<p id='baslik' title='Windows kısayolı 5'>Wi-Fi şifrenizi bulun</p> <br><p id='aciklama'> Windows'ta Ağ ve Paylaşım Merkezi'ne gidin. Wi-Fi ağ bağlantısı simgesi > Kablosuz Özellikleri'ne sağ tıklayın. Wi-Fi şifrenizi göstermek için <strong id='kyb' title='Bu işlem biraz garanti dışı!!!'>Karakterleri Göster</strong> kutusuna tıklayın.</p>";
+
+quotes[5] = "<p id='baslik' title='Windows kısayolı 6'>Windows Yetkili Kullanıcı Menüsü</p><br><p id='aciklama'>Bu menüye <strong id='kyb'>Windows tuşu + X" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight x'>X</span>" +
+    "</span>" +
+    "</strong>'e basılarak da erişilebilir.</p>";
+
+quotes[6] = "<p id='baslik' title='Windows kısayolı 7'>Windows gizli MandoTeam Modu</p><br><p id='aciklama'>Bu moda girmek için, Yeni bir klasör oluşturun ( Kodu kopyalayıp Yeni Klasör ismi ile değiştirin):<br /><strong id='kyb'>MandoTeam.{ED7BA470-8E54-465E-825C-99712043E01C}" +
+    "<span class='tooltip'>" +
+    "<span class='highlight mando'>MandoTeam</span>" +
+    "</span>" +
+    "</strong> Klasör simgesi, Kontrol Paneli tarzı bir simgeye dönüşecek ve her türlü ayarı açıp değiştirebileceksiniz.</p>";
+
+quotes[7] = "<p id='baslik' title='Windows kısayolı 8'>Bilgisayarınızı kilitleme</p><br> <p id='aciklama'><strong id='kyb'>Windows logo tuşu  + L" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight l'>L</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[8] = "<p id='baslik' title='Windows kısayolı 9'>Emoji Kısa yolu</p><br><p id='aciklama'>Windows için<br /><strong id='kyb'>Windows tuşu + Nokta" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight nokta'>.</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[9] = "<p id='baslik' title='Windows kısayolı 10'>Yer işareti eklemek </p> <br> <p id='aciklama' title='MandoTeam.BLoGsPoT.CoM'> En sevdiğiniz web sitenin sayfasını etiketleyip kolayca o siteye ulaşmanızı sağlayan bir yöntem var:<br />Bilgisayar için: <strong id='kyb'>Ctrl + D" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_ctrl'>Ctrl</span>" +
+    "<span class='highlight d'>D</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[10] = "<p id='baslik' title='Windows kısayolı 11'>Masaüstüne Geçiş</p><br><p id='aciklama'>Özellik ile masa üstü dolu nereye tıklayacağınızı şaşırmış durumdasınız kafa rahatlatıcı bir kısayol. Masaüstü göster ve gizle iki fonksiyonlu<br /><strong id='kyb'>Win + D" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight d'>D</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[11] = "<p id='baslik' title='Windows kısayolı 12'>Dosya Gezginini Aç</p><br><p id='aciklama'>Dosya yöneticisini açmak için klavyeden<br /><strong id='kyb'>Win + E" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight e'>E</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[12] = "<p id='baslik' title='Windows kısayolı 13'>Masaüstü Temizle</p><br><p id='aciklama'>Masaüstüne temizlemek için klavyeden<br /><strong id='kyb'>Win + M" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight m'>M</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[13] = "<p id='baslik' title='Windows kısayolı 14'>Ekran Yansıt</p><br><p id='aciklama'>Ekranı yansıtma ayarlarını açar<br /><strong id='kyb'>Win + P" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight p'>P</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[14] = "<p id='baslik' title='Windows kısayolı 15'>Tüm Pencereleri Göster</p><br><p id='aciklama'>Tüm açık pencereleri küçültüp masaüstünü göstermek için<br /><strong id='kyb'>Win + D" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_win'>Win</span>" +
+    "<span class='highlight d'>D</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[15] = "<p id='baslik' title='Windows kısayolı 16'>Açık Pencereler Arasında Geçiş</p><br><p id='aciklama'>Tüm açık pencereleri tıklamak zorunda kalmadan geçiş yapmak için<br /><strong id='kyb'>Alt + Tab" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_alt'>Alt</span>" +
+    "<span class='highlight tab'>Tab</span>" +
+    "</span>" +
+    "</strong></p>";
+
+quotes[16] = "<p id='baslik' title='Windows kısayolı 17'>Baskı Önizleme için</p><br><p id='aciklama'>Herhangi bir programda yazdırmak için <strong id='kyb'>Ctrl + P" +
+    "<span class='tooltip'>" +
+    "<span class='highlight left_ctrl'>Ctrl</span>" +
+    "<span class='highlight p'>P</span>" +
+    "</span>" +
+    "</strong> tuşlarına basabilirsiniz.</p>";
+
+quotes[17] = "<p id='baslik' title='Windows kısayolı 18'>Dosya Yöneticisine Ait Kısayollar</p><br><p id='aciklama'>Bu kısa yollar dosya gezginini hızlı bir şekilde kullanmanızı sağlar. Tek tek yazmak yerine hepsini tek bloğa sığdırmaya karar verdim:<br />" +
+    "<strong id='kyb'>Alt + D<span class='tooltip'><span class='highlight left_alt'>Alt</span><span class='highlight d'>D</span></span></strong> Dosya Gezgini'nde adres çubuğunu seç.<br />" +
+    "<strong id='kyb'>Alt + F<span class='tooltip'><span class='highlight left_alt'>Alt</span><span class='highlight f'>F</span></span></strong> Dosya menüsünü aç.<br />" +
+    "<strong id='kyb'>Alt + P<span class='tooltip'><span class='highlight left_alt'>Alt</span><span class='highlight p'>P</span></span></strong> Önizleme panelini aç.<br />" +
+    "<strong id='kyb'>Alt + T<span class='tooltip'><span class='highlight left_alt'>Alt</span><span class='highlight t'>T</span></span></strong> Görev çubuğuna atlama.<br />" +
+    "<strong id='kyb'>Alt + Tab<span class='tooltip'><span class='highlight left_alt'>Alt</span><span class='highlight tab'>Tab</span></span></strong> Açık uygulamalar arasında geçiş yap.<br />" +
+    "<strong id='kyb'>Alt + F4<span class='tooltip'><span class='highlight left_alt'>Alt</span><span class='highlight f4'>F4</span></span></strong> Etkin öğeyi kapat veya etkin uygulamadan çık.<br />" +
+    "<strong id='kyb'>Alt + Enter<span class='tooltip'><span class='highlight left_alt'>Alt</span><span class='highlight enter'>Enter</span></span></span></strong> Seçili öğenin özelliklerini görüntüle.<br />" +
+    "<strong id='kyb'>Alt + Esc<span class='tooltip'><span class='highlight left_alt'>Alt</span><span class='highlight esc'>Esc</span></span></strong> Öğeler arasında açıldıkları sırayla dolaş.<br />" +
+    "<strong id='kyb'>Win + 1<span class='tooltip'><span class='highlight right_win'>Win</span><span class='highlight one'>1</span></span></strong> Oturum açma alanında parolanızı görünür yapar.</p>";
+
+
+quotes[18] = "<p id='baslik' title='Windows kısayolı 19'>Sağ Tıklama</p><br><p id='aciklama'>Seçili nesne için sağ tıklama ile aynısı.<br><strong id='kyb' title='Shift + F10'>Shift + F10<span class='tooltip'><span class='highlight left_shift'>Shift</span><span class='highlight f10'>F10</span></span></strong></p>";
+
+
+
+    var rand1 = Math.floor(Math.random() * quotes.length);
+    var quote = quotes[rand1];
+    document.write(quote);
