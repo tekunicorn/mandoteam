f17eb65d95d5ad96d7f0ea4bc796cba3a2de7d7f
DOĞAN KASAPOĞLU <84666363+tekunicorn@users.noreply.github.com>
2024-10-01 21:56:23 +0300

    Update kizgin_script.js

--- kizgin_script.js
+++ kizgin_script.js
@@ -1,1 +1,30 @@
- eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('0.1(\'7\').6(\'b\',3(){0.1(\'5\').d.x(\'h\');0.1(\'7\').9.i=\'l\'});0.1(\'k\').6(\'b\',3(){0.1(\'5\').d.j(\'h\');0.1(\'7\').9.i=\'m\'});a c=0.n(\'4\');c.p(3(4){4.6(\'q\',3(){a 2=4.g(\'.2\');2.9.e=\'r\'});4.6(\'s\',3(){a 2=4.g(\'.2\');2.9.e=\'t\'})});0.6(\'b\',3(8){a 5=0.1(\'5\');u(!5.v(8.f)&&!8.f.w(\'#7\')){8.o()}});',34,34,'document|getElementById|tooltip|function|strong|popup|addEventListener|showPopup|event|style|var|click|strongs|classList|visibility|target|querySelector|active|display|remove|closePopup|none|block|querySelectorAll|stopPropagation|forEach|mouseenter|visible|mouseleave|hidden|if|contains|matches|add'.split('|'),0,{}));
+        document.getElementById('showPopup').addEventListener('click', function() {
+            document.getElementById('popup').classList.add('active');
+            document.getElementById('showPopup').style.display = 'none';  // Butonu gizle
+        });
+
+        document.getElementById('closePopup').addEventListener('click', function() {
+            document.getElementById('popup').classList.remove('active');
+            document.getElementById('showPopup').style.display = 'block';  // Butonu tekrar göster
+        });
+
+        var strongs = document.querySelectorAll('strong');
+
+        strongs.forEach(function(strong) {
+            strong.addEventListener('mouseenter', function() {
+                var tooltip = strong.querySelector('.tooltip');
+                tooltip.style.visibility = 'visible'; // Tooltip'i göster
+            });
+
+            strong.addEventListener('mouseleave', function() {
+                var tooltip = strong.querySelector('.tooltip');
+                tooltip.style.visibility = 'hidden'; // Tooltip'i gizle
+            });
+        });
+
+        document.addEventListener('click', function(event) {
+            var popup = document.getElementById('popup');
+            if (!popup.contains(event.target) && !event.target.matches('#showPopup')) {
+                event.stopPropagation();
+            }
+        });
