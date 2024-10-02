40e84aa6675eab5f05cb3ce25b8988345faa29b4
DOĞAN KASAPOĞLU <84666363+tekunicorn@users.noreply.github.com>
2024-10-02 17:23:03 +0300

    Update kizgin_script.js

--- kizgin_script.js
+++ kizgin_script.js
@@ -1,13 +1,16 @@
 strongs.forEach(function(strong) {
-    strong.addEventListener('mouseenter', function() {
-        var tooltip = strong.querySelector('.tooltip');
-        tooltip.style.visibility = 'visible'; // Tooltip'i göster
-    });
+    if (window.innerWidth > 768) { // Masaüstü için
+        strong.addEventListener('mouseenter', function() {
+            var tooltip = strong.querySelector('.tooltip');
+            tooltip.style.visibility = 'visible'; // Tooltip'i göster
+        });
 
-    strong.addEventListener('mouseleave', function() {
-        var tooltip = strong.querySelector('.tooltip');
-        tooltip.style.visibility = 'hidden'; // Tooltip'i gizle
-    });
+        strong.addEventListener('mouseleave', function() {
+            var tooltip = strong.querySelector('.tooltip');
+            tooltip.style.visibility = 'hidden'; // Tooltip'i gizle
+        });
+    }
+});
 
     // Mobil için
     strong.addEventListener('touchstart', function() {
